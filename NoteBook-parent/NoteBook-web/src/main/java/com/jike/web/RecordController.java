3b52d43066348ed23e0de187a3e265c1cd8a5ca7
ZGHS <1768702303@qq.com>
2018-05-08 08:21:10 +0800

    7899

--- NoteBook-parent/NoteBook-web/src/main/java/com/jike/web/RecordController.java
+++ NoteBook-parent/NoteBook-web/src/main/java/com/jike/web/RecordController.java
@@ -4,15 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSON;
-import com.jike.entity.AccountInfo;
 import com.jike.entity.Record;
-import com.jike.service.AccountInfoService;
 import com.jike.service.RecordService;
 
 @Controller
